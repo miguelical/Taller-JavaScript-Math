1e124b8196176b6b361a6d16db63c9620cf546a1
Miguel Ical <miguel.ical21@gmail.com>
2023-04-29 13:15:16 -0600

    Uso de métodos de arrays

--- descuentos.js
+++ descuentos.js
@@ -6,6 +6,18 @@ const pResult = document.querySelector('#result');
 
 btn.addEventListener('click', calcularDescuento);
 
+// const couponsObj = {
+//   'JuanDC_es_Batman': 30,
+//   '3456789': 25,
+//   '123': 15,
+// };
+
+const couponsList = [];
+couponsList.push({
+  name: 'JuanDC',
+  discount: 30,
+});
+
 function calcularDescuento() {
   const price = Number(inputPrice.value);
   const coupon = inputCoupon.value;
@@ -18,18 +30,40 @@ function calcularDescuento() {
 
   let discount;
 
-    switch (coupon) {
-      case 'Premium':
-        discount = 30;
-        break;
-      case 'Verano':
-        discount = 25;
-        break;
-
-      default:
-        pResult.innerText = 'El cupón no es válido';
-        return;
+    function isCouponInArray(couponElement) {
+      return couponElement.name == coupon;
     };
+
+    const couponInArray = couponsList.find(isCouponInArray);
+
+    if (couponInArray) {
+      discount = couponInArray.discount;
+    } else {
+      pResult.innerText = 'El cupón no es válido';
+      return;
+    }
+
+
+    // if (couponsObj[coupon]) {
+    //   discount = couponsObj[coupon];
+    // } else {
+    //   pResult.innerText = 'El cupón no es válido';
+    //   return;
+    // };
+
+
+    // switch (coupon) {
+    //   case 'Premium':
+    //     discount = 30;
+    //     break;
+    //   case 'Verano':
+    //     discount = 25;
+    //     break;
+
+    //   default:
+    //     pResult.innerText = 'El cupón no es válido';
+    //     return;
+    // };
 
     const newPrice = (price * (100 - discount)) / 100;
 
